134fe9f296a1fb22776d673bb1028c10cb4b7dee
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-06-19 23:35:26 +0900

    feat: integration graphQl module

--- apps/hankang-batch/src/hankang-batch.module.ts
+++ apps/hankang-batch/src/hankang-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HankangBatchController } from './hankang-batch.controller';
 import { HankangBatchService } from './hankang-batch.service';
+import { ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [HankangBatchController],
   providers: [HankangBatchService],
 })
--- apps/hankang-batch/src/main.ts
+++ apps/hankang-batch/src/main.ts
@@ -3,6 +3,6 @@ import { HankangBatchModule } from './hankang-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(HankangBatchModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT_BATCH ?? 4000);
 }
 bootstrap();
