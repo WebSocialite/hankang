c9f8b16540a36bc8c06e961a6541ef9fb72ae35e
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-06-19 23:54:22 +0900

    feat: build project standards

--- apps/hankang-api/src/app.module.ts
+++ apps/hankang-api/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver } from "@nestjs/apollo";
+import { AppResolver } from './app.resolver';
+import { ComponentsModule } from '../components/components.module';
+import { DatabaseModule } from '../database/database.module';
+// import { ComponentsModule } from './components/components.module';
+// import { DatabaseModule } from './database/database.module';
+// import { T } from './libs/types/common';
+// import { SocketModule } from './socket/socket.module';
 
-@Module({
-  imports: [],
+@Module({ //GRAPH QL API BACKEND SERVERINI TASHKIL QILYAPMIZ!!!!!
+  imports: [
+    ConfigModule.forRoot(), // envorimental variablelarni oqish imkoniyatinin beradi
+    GraphQLModule.forRoot({ // graphQL ni yasavolayapmiz
+      driver: ApolloDriver,
+      playground: true,
+      uploads: false,
+      autoSchemaFile: true,
+      // formatError: (error: ) => {
+      //   const graphQLFormattedError = {
+      //     code: error?.extensions.code,
+      //     message: error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message, 
+      //   };
+      //   console.log("GRAPHQL GLOBAL ERR", graphQLFormattedError);
+      //   return graphQLFormattedError;
+      // },
+    }),
+    ComponentsModule,
+    DatabaseModule,
+    
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- apps/hankang-api/src/app.service.ts
+++ apps/hankang-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'WELCOME TO HANKANG API SERVER!';
+    return 'WELCOME TO HANKANG REST API SERVER!';
   }
 }
