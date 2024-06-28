b0bce4df2947984532fb4ce3b02b01e3d92f7ac0
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-06-28 11:56:03 +0900

    feat: standard integration of LoggingInterceptor

--- apps/hankang-api/src/main.ts
+++ apps/hankang-api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());   // user o'zini datasini tugri kiritgan yoki yoq ligini validate qlib beradi  
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT_API ?? 4000);
 }
 bootstrap();
