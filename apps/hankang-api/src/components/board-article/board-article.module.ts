3677f0d0aec77bb6859a90a89ce0772228ded976
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-07-02 22:31:43 +0900

    feat: develop BoardArticle related

--- apps/hankang-api/src/components/board-article/board-article.module.ts
+++ apps/hankang-api/src/components/board-article/board-article.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { BoardArticleResolver } from './board-article.resolver';
+import { BoardArticleService } from './board-article.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import BoardArticleSchema from '../../schemas/BoardArticle.model';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { ViewModule } from '../view/view.module';
+import { LikeModule } from '../like/like.module';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'BoardArticle',  // chech whether it starts with Captal letter
+        schema: BoardArticleSchema,
+      },
+    ]),
+    AuthModule,
+    MemberModule,  // we will use statistic update method of users
+    ViewModule,   
+    LikeModule, 
+  ],
+  providers: [BoardArticleResolver, BoardArticleService],
+  exports: [BoardArticleService],
+})
 export class BoardArticleModule {}
